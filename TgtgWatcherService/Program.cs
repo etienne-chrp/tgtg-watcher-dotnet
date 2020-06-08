1a1f239544673c210d32d650f6d3451b5a164907
etienne <etienne@local>
2020-06-08 11:59:49 +0200

    Refactorize service to use ASP.Net Worker

--- TgtgWatcherService/Program.cs
+++ TgtgWatcherService/Program.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace TgtgWatcherService
 {
-    class Program
+    public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var hostBuilder = new HostBuilder()
-            .ConfigureAppConfiguration((hostContext, configBuilder) =>
-            {
-                configBuilder.SetBasePath(Directory.GetCurrentDirectory());
-                configBuilder.AddJsonFile("appsettings.json", optional: true);
-                configBuilder.AddJsonFile(
-                    $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
-                    optional: true);
-                configBuilder.AddEnvironmentVariables();
-            })
-            .ConfigureLogging((hostContext, configLogging) =>
-            {
-                configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
-                configLogging.AddConsole();
-            })
-            .ConfigureServices((hostContext, services) =>
-            {
-                // Here goes your internal application dependencies
-                // like EntityFramework context, worker, endpoint, etc.
-                services.AddOptions();
-                services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
-                services.AddScoped<IHostedService, TimedHostedService>();
-            });
-
-            await hostBuilder.RunConsoleAsync();            
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                {
+                    configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    configBuilder.AddJsonFile("appsettings.json", optional: true);
+                    configBuilder.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    configBuilder.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    configLogging.AddConsole();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Here goes your internal application dependencies
+                    // like EntityFramework context, worker, endpoint, etc.
+                    services.AddOptions();
+                    services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
+                    services.AddHostedService<TimedHostedService>();
+                });
     }
 }
